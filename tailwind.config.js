d43faf407cae010fa7d3993a93f3d1386728e66b
Tebogo Nomnqa <tebzax2@gmail.com>
2023-01-14 13:49:34 +0200

    feat: customize and config Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ module.exports = {
     "src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl:"1440px",
+    },
+    extend: {
+      colors: {
+        //Primary
+        brightRed: "hsl(12, 88%, 59%)",
+        brightRedLight: "hsl(12, 88%, 69%)",
+        brightRedSupLight: "hsl(12, 88%, 99%)",
+        darkBlue: "hsl(228, 39%, 23%)",
+        //Neutral
+        darkGrayishBlue: "hsl(227, 12%, 61%)",
+        veryDarkBlue: "hsl(233, 12%, 13%) ",
+        veryPaleRed: "hsl(13, 100%, 96%) ",
+        VaryLightGray: "hsl(0, 0%, 98%)",
+      }
+    },
   },
   plugins: [],
 }
