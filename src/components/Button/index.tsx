25a6ffa2ddb482dfdf9583d322368a16d20322d2
Tebogo Nomnqa <tebzax2@gmail.com>
2023-01-16 12:23:03 +0200

    feat: add secondary intent variant for Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,38 @@
 import React from "react";
 import Link from "next/link";
+import { cva, type VariantProps } from "class-variance-authority";
 
-interface Props {
+interface Props
+  extends VariantProps<typeof ButtonStyles>,
+    React.HTMLAttributes<HTMLAnchorElement> {
   title: string;
   href: string;
-  className?: string;
 }
-export default function Button({ title, href, className }: Props) {
+const ButtonStyles = cva("rounded-full shadow-lg px-8 pt-4 pb-3 font-bold", {
+  variants: {
+    intent: {
+      primary:
+        "bg-brightRed text-white shadow-brightRedLight hover:bg-brightRedLight",
+      secondary: "bg-white text-brightRed hover:text-brightRedLight",
+    },
+  },
+  defaultVariants: {
+    intent: "primary",
+  },
+});
+
+export default function Button({
+  title,
+  href,
+  className,
+  intent,
+  ...props
+}: Props) {
   return (
     <Link
       href={href}
-      className={`bg-brightRed rounded-full px-8 pt-4 pb-3 text-white shadow-lg shadow-brightRedLight hover:bg-brightRedLight ${className}`}
+      className={ButtonStyles({ className, intent })}
+      {...props}
     >
       {title}
     </Link>
